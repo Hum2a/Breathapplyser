91062ca871427e3b063e49ea94abf83e6694cab2
Humza Butt <72743279+UP942033@users.noreply.github.com>
2024-03-20 20:25:46 +0000

    Drunkness Chart Created

--- src/frontend/components/charts/linecharts/IndividualCharts/DrunknessChart.js
+++ src/frontend/components/charts/linecharts/IndividualCharts/DrunknessChart.js
@@ -6,9 +6,10 @@ import { collection, getFirestore, query, orderBy, getDocs } from 'firebase/fire
 import { UserContext } from '../../../../context/UserContext';
 import moment from 'moment';
 import { chartConfig } from '../chart-handling/chartConfig';
-import { drunkStyles as styles } from '../../../styles/ChartStyles/DrunknessStyles';
+import { DrunkStyles as styles } from '../../../styles/ChartStyles/DrunknessStyles';
+import { getDrunkennessLevel } from '../../../../../backend/app/background/Drunkness/drunknessCalculator';
 
-const BACIncreaseChart = () => {
+const DrunkennessLevelChart = () => {
     const [bacIncreaseValues, setBacIncreaseValues] = useState([]);
     const [chartLabels, setChartLabels] = useState([]);
     const [allEntries, setAllEntries] = useState([]);
@@ -16,25 +17,9 @@ const BACIncreaseChart = () => {
     const [comparisonMode, setComparisonMode] = useState(false);
     const [selectedDate2, setSelectedDate2] = useState('');
     const [bacIncreaseValues2, setBacIncreaseValues2] = useState([]);
-    const [markersData, setMarkersData] = useState([]); // Store data for markers
-
+    const [drunkennessLevel, setDrunkenessLevel] = useState([]);
     const firestore = getFirestore();
     const { user } = useContext(UserContext);
-
-    // Define markers for different levels of drunkenness
-    const drunkennessMarkers = [
-        { label: 'Sober', bacLevel: 0.01 },
-        { label: 'Buzzed', bacLevel: 0.03 },
-        { label: 'Relaxed', bacLevel: 0.10 },
-        { label: 'A Bit of a liability', bacLevel: 0.15 },
-        { label: 'Visibly Drunk', bacLevel: 0.20 },
-        { label: 'Embarassing', bacLevel: 0.25 },
-        { label: 'Sickly', bacLevel: 0.30 },
-        { label: 'Either pull or go home', bacLevel: 0.35 },
-        { label: 'Find a friend', bacLevel: 0.40 },
-        { label: 'Gonna Pass out', bacLevel: 0.45 },
-        { label: 'Call an Ambulance', bacLevel: 0.50 },
-    ];
 
     useEffect(() => {
         const fetchAllEntries = async () => {
@@ -67,87 +52,56 @@ const BACIncreaseChart = () => {
     }, []);
 
     const filterDataByDate = (entries, date, date2 = '') => {
-      setSelectedDate(date);
-      const filteredEntries = entries.filter(entry => entry.date === date);
-      const newValues = [];
-      const labels = [];
-      let cumulativeBACIncrease = 0;
-  
-      // Reset markers data for each new set of entries
-      setMarkersData([]);
-  
-      filteredEntries.sort((a, b) => moment(a.startTime, 'YYYY-MM-DD HH:mm:ss').diff(moment(b.startTime, 'YYYY-MM-DD HH:mm:ss')));
-  
-      filteredEntries.forEach(entry => {
-          cumulativeBACIncrease += parseFloat(entry.BACIncrease || 0);
-          newValues.push(cumulativeBACIncrease);
-          const timeLabel = entry.startTime ? moment(entry.startTime, 'YYYY-MM-DD HH:mm:ss').format('HH:mm') : 'Unknown Time';
-          labels.push(timeLabel);
-      });
-  
-      setBacIncreaseValues(newValues);
-      setChartLabels(labels);
-  
-      // Calculate crossing points for markers
-      drunkennessMarkers.forEach(marker => {
-          const crossingPoint = findCrossingPoint(newValues, marker.bacLevel);
-          if (crossingPoint !== null) {
-              setMarkersData(prevData => [...prevData, { label: marker.label, value: crossingPoint }]);
-          }
-      });
-  
-      if (comparisonMode && date2) {
-          setSelectedDate2(date2);
-          const filteredEntries2 = entries.filter(entry => entry.date === date2);
-          const newValues2 = [];
-          let cumulativeBACIncrease2 = 0;
-  
-          filteredEntries2.forEach(entry => {
-              cumulativeBACIncrease2 += parseFloat(entry.bacIncrease || 0);
-              newValues2.push(cumulativeBACIncrease2);
-          });
-  
-          setBacIncreaseValues2(newValues2);
-      }
-  };
+        setSelectedDate(date);
+        const filteredEntries = entries.filter(entry => entry.date === date);
+        const newValues = [];
+        const labels = [];
+        const drunknessLevels = {}; // Object to store the time at which each level of drunkness was reached
     
-    const findCrossingPoint = (data, threshold) => {
-        for (let i = 1; i < data.length; i++) {
-            if (data[i - 1] < threshold && data[i] >= threshold) {
-                // Interpolate the exact crossing point
-                const t = (threshold - data[i - 1]) / (data[i] - data[i - 1]);
-                return i - 1 + t;
+        let cumulativeBACIncrease = 0;
+    
+        filteredEntries.sort((a, b) => moment(a.startTime, 'YYYY-MM-DD HH:mm:ss').diff(moment(b.startTime, 'YYYY-MM-DD HH:mm:ss')));
+    
+        filteredEntries.forEach(entry => {
+            cumulativeBACIncrease += parseFloat(entry.BACIncrease || 0);
+            newValues.push(cumulativeBACIncrease);
+            const timeLabel = entry.startTime ? moment(entry.startTime, 'YYYY-MM-DD HH:mm:ss').format('HH:mm') : 'Unknown Time';
+            labels.push(timeLabel);
+    
+            // Check if the current level of drunkness has been reached
+            const level = getDrunkennessLevel(cumulativeBACIncrease).simple;
+            if (!(level in drunknessLevels)) {
+                drunknessLevels[level] = timeLabel; // Store the time at which the level of drunkness was reached
             }
+        });
+    
+        setBacIncreaseValues(newValues);
+        setChartLabels(labels);
+    
+        if (comparisonMode && date2) {
+            setSelectedDate2(date2);
+            const filteredEntries2 = entries.filter(entry => entry.date === date2);
+            const newValues2 = [];
+            let cumulativeBACIncrease2 = 0;
+    
+            filteredEntries2.forEach(entry => {
+                cumulativeBACIncrease2 += parseFloat(entry.bacIncrease || 0);
+                newValues2.push(cumulativeBACIncrease2);
+            });
+    
+            setBacIncreaseValues2(newValues2);
         }
-        return null; // No crossing point found
+    
+        console.log('Drunkness Levels:', drunknessLevels); // Print the drunkness levels and their corresponding times
+        setDrunkenessLevel(drunknessLevels);
     };
-    const calculateMarkerPosition = (value) => {
-      // Ensure that chartConfig properties are defined
-      if (chartConfig.min !== undefined && chartConfig.max !== undefined && chartConfig.height !== undefined) {
-          // Calculate the percentage of the value within the range
-          const percentage = (value - chartConfig.min) / (chartConfig.max - chartConfig.min);
-  
-          // Calculate the y-position by multiplying the percentage with the chart height
-          const yPosition = chartConfig.height * (1 - percentage); // Invert the percentage to ensure correct positioning
-  
-          // Logging calculated positions
-          console.log(`Marker Value: ${value}`);
-          console.log(`Calculated Y Position: ${yPosition}`);
-  
-          return { y: yPosition };
-      } else {
-          // Return {y: 0} if chartConfig properties are not defined
-          return { y: 0 };
-      }
-  };
-  
-  
+    
 
     const uniqueDates = [...new Set(allEntries.map(entry => entry.date))];
 
     return (
         <View style={styles.container}>
-            <Text style={styles.graphTitle}>BAC Cumulative Increase Chart</Text>
+            <Text style={styles.graphTitle}>Drunkness Chart</Text>
             <Switch
                 value={comparisonMode}
                 onValueChange={() => {
@@ -157,15 +111,15 @@ const BACIncreaseChart = () => {
                 }}
             />
             <View style={styles.pickersContainer}>
-                <Picker
-                    selectedValue={selectedDate}
-                    onValueChange={(itemValue) => filterDataByDate(allEntries, itemValue)}
-                    style={styles.pickerStyle}
-                >
-                    {uniqueDates.map(date => (
-                        <Picker.Item key={date} label={date} value={date} />
-                    ))}
-                </Picker>
+              <Picker
+                  selectedValue={selectedDate}
+                  onValueChange={(itemValue) => filterDataByDate(allEntries, itemValue)}
+                  style={styles.pickerStyle}
+              >
+                  {uniqueDates.map(date => (
+                      <Picker.Item key={date} label={date} value={date} />
+                  ))}
+              </Picker>
             </View>
             {comparisonMode && (
                 <Picker
@@ -183,41 +137,41 @@ const BACIncreaseChart = () => {
             )}
             {bacIncreaseValues.length > 0 ? (
                 <View>
-                <LineChart
-                  data={{
-                      labels: chartLabels,
-                      datasets: [
-                          { data: bacIncreaseValues, color: () => '#2979FF' },
-                          ...(comparisonMode && bacIncreaseValues2.length > 0
-                              ? [{ data: bacIncreaseValues2, color: () => '#FF6D00' }]
-                              : [])
-                      ],
-                  }}
-                  width={350}
-                  height={200}
-                  chartConfig={{
-                      ...chartConfig,
-                      backgroundColor: '#ffffff',
-                      backgroundGradientFrom: '#ffffff',
-                      backgroundGradientTo: '#ffffff',
-                      decimalPlaces: 2,
-                      color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-                      style: { borderRadius: 16 },
-                  }}
-                  bezier
-                  fromZero
-                  yAxisLabel={({ value }) => {
-                      // Find the drunkenness label corresponding to the value
-                      const drunkennessLabel = drunkennessMarkers.find(marker => value >= marker.bacLevel)?.label || '';
-                      return drunkennessLabel;
-                  }}
-              />
-
-                <View style={styles.legendContainer}>
-                    <View style={[styles.legendItem, { backgroundColor: '#2979FF' }]} />
-                    <Text style={styles.legendLabel}>BAC Increase</Text>
+                    <LineChart
+                        data={{
+                            labels: chartLabels,
+                            datasets: [
+                                { data: bacIncreaseValues, color: () => '#2979FF' },
+                                ...(comparisonMode && bacIncreaseValues2.length > 0
+                                    ? [{ data: bacIncreaseValues2, color: () => '#FF6D00' }]
+                                    : [])
+                            ],
+                        }}
+                        width={350}
+                        height={200}
+                        chartConfig={{
+                            ...chartConfig,
+                            backgroundColor: '#ffffff',
+                            backgroundGradientFrom: '#ffffff',
+                            backgroundGradientTo: '#ffffff',
+                            decimalPlaces: 2,
+                            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                            style: { borderRadius: 16 },
+                        }}
+                        bezier
+                        fromZero
+                    />
+                    <View style={styles.legendContainer}>
+                        <View style={[styles.legendItem, { backgroundColor: '#2979FF' }]} />
+                        <Text style={styles.legendLabel}>BAC Increase</Text>
+                    </View>
+                    {Object.entries(drunkennessLevel).map(([level, time]) => (
+                        <View key={level} style={styles.drunknessLevelContainer}>
+                            <Text style={styles.drunknessLevel}>{level}</Text>
+                            <Text style={styles.drunknessTime}>{time}</Text>
+                        </View>
+                    ))}
                 </View>
-            </View>            
             ) : (
                 <Text style={styles.noDataText}>No data available for this date.</Text>
             )}
@@ -225,4 +179,4 @@ const BACIncreaseChart = () => {
     );
 }
 
-export default BACIncreaseChart;
+export default DrunkennessLevelChart;
--- src/frontend/components/styles/ChartStyles/DrunknessStyles.js
+++ src/frontend/components/styles/ChartStyles/DrunknessStyles.js
@@ -1,96 +1,74 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
-export const drunkStyles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingHorizontal: 20, // Adjust the horizontal padding
-      paddingTop: 40, // Add padding top for spacing
-    },
-    pickersContainer: {
-      flexDirection: 'row',
-      marginTop: 5, // Add margin top for spacing
-      backgroundColor: 'transparent',
-      borderRadius: 5,
-      padding: 5,
-    },
-    pickerStyle: {
-      flex: 1,
-      height: 40,
-      marginHorizontal: 5, // Adjust horizontal margin for spacing
-      borderWidth: 10,
-      borderColor: '#29B6F6', // Light blue border color
-      borderRadius: 50,
-      backgroundColor: '#E1F5FE', // Lighter blue background for the picker
-    },
-    legendContainer: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: 20, // Add margin top for spacing
-    },
-    legendItem: {
-      width: 10,
-      height: 10,
-      borderRadius: 5,
-      marginRight: 5,
-    },
-    legendLabel: {
-      fontSize: 14,
-      color: '#01579b', // Using the darker blue for better readability
-    },
-    graphTitle: {
-      color: '#01579b', // Darker blue for the title text for contrast
-      fontSize: 18, // Slightly larger font size for better visibility
-      fontWeight: 'bold',
-      textAlign: 'center',
-      marginBottom: 10,
-    },
-    toggleButton: {
-      backgroundColor: '#29b6f6', // A light blue color for the button
-      padding: 10,
-      borderRadius: 5,
-      alignItems: 'center',
-      marginVertical: 10,
-      shadowColor: "#000", // Optional: adding shadow for better depth
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      elevation: 5,
-    },
-    toggleButtonText: {
-      color: '#ffffff', // Keeping the text color white for contrast
-      fontSize: 16,
-      fontWeight: 'bold',
-    },
-    marker: {
-        position: 'absolute',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        left: 0,
-        right: 0,
-        zIndex: 100,
-      },
-      markerLabel: {
-        fontSize: 12,
-        fontWeight: 'bold',
-        marginRight: 5,
-      },
-      circle: {
-        position: 'absolute',
-        width: 10,
-        height: 10,
-        borderRadius: 5,
-        backgroundColor: 'red', // Adjust circle color as needed
-      },
-      noDataText: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        marginTop: 20,
-      },
+export const DrunkStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E0F2F1', // Light blue background color
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  graphTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#1565C0', // Dark blue text color
+  },
+  pickersContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    alignSelf: 'center',
+  },
+  pickerStyle: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#B3E5FC', // Light blue picker background color
+    borderRadius: 10,
+    marginRight: 10,
+    alignSelf: 'center',
+  },
+  legendContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    alignSelf: 'center',
+  },
+  legendItem: {
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    marginRight: 5,
+    alignSelf: 'center',
+  },
+  legendLabel: {
+    fontSize: 14,
+    color: '#1565C0', // Dark blue legend text color
+    alignSelf: 'center',
+  },
+  drunknessLevelContainer: {
+    flexDirection: 'row',
+    marginBottom: 5,
+    backgroundColor: '#B3E5FC', // Light blue background color for the row
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  drunknessLevel: {
+    fontWeight: 'bold',
+    marginRight: 10,
+    color: '#1565C0', // Dark blue level text color
+    alignSelf: 'center',
+  },
+  drunknessTime: {
+    fontStyle: 'italic',
+    color: '#1565C0', // Dark blue time text color
+    alignSelf: 'center',
+  },
+  noDataText: {
+    fontSize: 16,
+    fontStyle: 'italic',
+    color: '#1565C0', // Dark blue no data text color
+    alignSelf: 'center',
+  },
 });
+
