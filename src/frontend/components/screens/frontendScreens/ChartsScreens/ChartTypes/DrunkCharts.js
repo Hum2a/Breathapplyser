be9691b439646abceb16171bb96bdfa729474879
Humza Butt <72743279+UP942033@users.noreply.github.com>
2024-05-01 14:53:12 +0100

    custom back button

--- src/frontend/components/screens/frontendScreens/ChartsScreens/ChartTypes/DrunkCharts.js
+++ src/frontend/components/screens/frontendScreens/ChartsScreens/ChartTypes/DrunkCharts.js
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
-import { View, Text, ScrollView, SafeAreaView, TouchableOpacity } from 'react-native';
+import { View, Text, ScrollView, SafeAreaView, TouchableOpacity, Image } from 'react-native';
 import { DrunkScreenStyles } from '../../../../styles/ChartStyles/DrunknessStyles';
 import { DrunknessENTRIESChart, DrunknessBLChart } from '../../../../charts/chartIndex';
 import { BackButton } from '../../../../buttons/backButton';
 
-const DrunkCharts = () => {
+const DrunkCharts = ({ navigation }) => {
   const [showEntriesChart, setShowEntriesChart] = useState(false); // State to control visibility of entries chart
   const [showBLChart, setShowBLChart] = useState(false); // State to control visibility of BL chart
+
+  const back = () => {
+    navigation.pop(1);
+  };
 
   return (
     <SafeAreaView style={DrunkScreenStyles.fullScreen}>
       <ScrollView style={DrunkScreenStyles.chartContainer}>
-        <BackButton />
+        <TouchableOpacity style={DrunkScreenStyles.iconContainer} onPress={back}>
+          <Image source={require('../../../../../assets/images/back_arrow.png')} style={DrunkScreenStyles.icon} />
+        </TouchableOpacity>
         {/* Toggle button for Entries chart */}
         <TouchableOpacity onPress={() => setShowEntriesChart(!showEntriesChart)} style={DrunkScreenStyles.button}>
           <Text style={DrunkScreenStyles.buttonText}>{showEntriesChart ? 'Hide Entries Chart' : 'Show Entries Chart'}</Text>
