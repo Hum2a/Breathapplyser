43101b4d296eb132d397b7614ac901feedbabd43
Humza Butt <72743279+UP942033@users.noreply.github.com>
2024-05-07 19:38:49 +0100

    Updated Drunkness Level Tracking

--- src/backend/firebase/queries/saveEntry.js
+++ src/backend/firebase/queries/saveEntry.js
@@ -1,75 +1,80 @@
 import { getFirestore, doc, setDoc, getDoc, updateDoc, increment } from 'firebase/firestore';
 import moment from 'moment';
 import calculateBACIncrease from '../../../frontend/utils/calculations/calculateBAC';
-import { bacLabels } from '../../app/background/Drunkness/BACLabels';
-import { getDrunkennessLevel } from '../../app/background/Drunkness/drunknessCalculator';
 
 const firestore = getFirestore();
 
-export const saveEntry = async (user, userProfile, entryDetails) => {
-  const { alcohol, amount, units, price, type, calories, selectedStartTime, selectedEndTime, selectedDate, selectedCurrency } = entryDetails;
 
+export const saveEntry = async (user, userProfile, entryDetails) => {
   if (!user) {
     console.error("User data is not available");
     return;
   }
 
-  // Calculate BAC increase
+  const getDrunkennessLevel = async (totalBAC) => {
+    const levelsRef = doc(getFirestore(), user.uid, 'Drunk Parameters');
+    try {
+      const docSnap = await getDoc(levelsRef);
+      if (docSnap.exists()) {
+        const levels = docSnap.data().levels;
+        // Assuming levels are sorted by upper limit of BAC and are continuous ranges
+        const level = levels.find(l => {
+          const [lower, upper] = l.range.split(' - ').map(parseFloat);
+          return totalBAC >= lower && totalBAC < upper;
+        });
+        return level ? level.simple : 'Unknown'; // Default to 'Unknown' if no level is matched
+      }
+      return 'Unknown'; // Default level if no data is available
+    } catch (error) {
+      console.error('Error fetching drunkness levels:', error);
+      return 'Unknown'; // Default level on error
+    }
+  };
+  
+
+  const { alcohol, amount, units, price, type, selectedStartTime, selectedEndTime, selectedDate, selectedCurrency } = entryDetails;
   const BACIncrease = calculateBACIncrease(units, userProfile);
 
-  // Get total BAC level for the selected date
   const dateStr = moment(selectedDate).format('YYYY-MM-DD');
   const bacLevelRef = doc(firestore, user.uid, "Daily Totals", "BAC Level", dateStr);
-  let drunknessLevel = 'Sober'; // Default drunkness level
 
   try {
     const bacLevelDoc = await getDoc(bacLevelRef);
     let totalBACLevel = bacLevelDoc.exists() ? bacLevelDoc.data().value : 0;
-
-    // Calculate new total BAC level
     totalBACLevel += BACIncrease;
 
-    // Determine drunkness level based on the total BAC level
-    drunknessLevel = getDrunkennessLevel(totalBACLevel).simple;
+    const drunknessLevel = await getDrunkennessLevel(totalBACLevel);  // Fetch drunkness level based on updated BAC level
 
-    // // Update total BAC level in Firestore
-    // await updateDoc(bacLevelRef, {
-    //   value: increment(BACIncrease), // Increment the total BAC level
-    //   lastUpdated: new Date()
-    // });
+    // Update total BAC level in Firestore
+    await updateDoc(bacLevelRef, {
+      value: increment(BACIncrease),
+      lastUpdated: new Date()
+    });
 
-  } catch (error) {
-    console.error('Error fetching or updating BAC level:', error);
-  }
+    // Prepare and save entry document
+    const entryDoc = {
+      user_id: user.uid,
+      alcohol,
+      amount: parseInt(amount),
+      units: parseFloat(units),
+      price: parseFloat(price),
+      type,
+      selectedCurrency,
+      startTime: moment(selectedStartTime, 'HH:mm').toISOString(),
+      endTime: moment(selectedEndTime, 'HH:mm').toISOString(),
+      BACIncrease,
+      drunknessLevel,
+      date: selectedDate,
+      timestamp: new Date(),
+    };
 
-  const entryDoc = {
-    user_id: user.uid,
-    alcohol,
-    amount: parseInt(amount),
-    units: parseFloat(units),
-    price: parseFloat(price),
-    selectedCurrency,
-    type,
-    calories,
-    startTime: moment(selectedStartTime, 'HH:mm').toISOString(),
-    endTime: moment(selectedEndTime, 'HH:mm').toISOString(),
-    BACIncrease,
-    drunknessLevel, // Add drunkness level to entry document
-    date: selectedDate,
-    timestamp: new Date(), // Current date and time
-  };
-
-  const entryDocId = moment().format('YYYYMMDD_HHmmss'); // Use a unique ID for each entry
-
-  try {
-    // Add or update a field in the dateStr document
+    const entryDocId = moment().format('YYYYMMDD_HHmmss');
     const dateStrDocRef = doc(firestore, user.uid, "Alcohol Stuff", "Entries", dateStr);
     await setDoc(dateStrDocRef, { lastUpdated: new Date() }, { merge: true });
-
-    // Create a nested structure for the entry document
     await setDoc(doc(dateStrDocRef, "EntryDocs", entryDocId), entryDoc);
+
     console.log('Entry saved successfully');
   } catch (error) {
-    console.error('Error saving entry:', error);
+    console.error('Error in saving entry:', error);
   }
 };
